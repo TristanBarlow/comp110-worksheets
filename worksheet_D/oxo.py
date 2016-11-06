20d0ac13d6533367fdb32bda3cef0e5e95003096
tristanbarlow <tristanbarlowg@gmail.com>
2016-11-06 19:51:52 +0000

    removed useless code

--- worksheet_D/oxo.py
+++ worksheet_D/oxo.py
@@ -1,7 +1,5 @@
 class OxoBoard:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
         self.board = {(0, 0): 0,(1, 0): 0, (2, 0): 0,
                       (0, 1): 0, (1, 1): 0, (2, 1): 0,
                       (0, 2): 0, (1, 2): 0, (2, 2): 0}      # Creates dictionary.
